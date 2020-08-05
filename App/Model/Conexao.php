60602b59d8f8da9dbe4e30f0307acd9813fb0c15
Michael Douglas do Amarante <doug.amarante@gmail.com>
2020-08-05 12:47:17 -0300

    update insert

--- App/Model/Conexao.php
+++ App/Model/Conexao.php
@@ -8,6 +8,7 @@
             if(!isset(self::$conexao)):
                 try{
                     self::$conexao = new \PDO('mysql:host=localhost;dbname=db_atasystem;charset=utf8','root','');
+                    self::$conexao->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
                 }catch(PDOException $e){
                     echo "Database error: ".$e->getMessage();
                 }catch(Exception $e){
--- App/Model/Insert.php
+++ App/Model/Insert.php
@@ -3,9 +3,11 @@
     require_once './../../vendor/autoload.php';
     
     use \GUMP;
+    ##Insert::registerUser('user','email@gmail.com','1234');
+    
     class Insert{
 
-        #### Insert::registerUser('user','email@gmail.com','1234');
+        
         public static function registerUser($name,$email,$passwd){
             $rules = [
                 'name' => 'required|max_len,100|min_len,3',
@@ -20,19 +22,19 @@
             $is_valid = GUMP::is_valid($user_data,$rules);
 
             if($is_valid === true):
-                $sql = 'INSERT INTO usuario(nome,email,senha) VALUES(?,?,?)';
-                $insert = Conexao::getConn()->prepare($sql);
-                $insert->bindValue(1,$user_data['name']);
-                $insert->bindValue(2,$user_data['email']);
-                $insert->bindValue(3,$user_data['passwd']);
+                    $sql = 'INSERT INTO usuario(nome,email,senha) VALUES(?,?,?)';
+                    $insert = Conexao::getConn()->prepare($sql);
+                    $insert->bindValue(1,$user_data['name']);
+                    $insert->bindValue(2,$user_data['email']);
+                    $insert->bindValue(3,$user_data['passwd']);
+                    
                 
-                $insert->execute();
-               
+                    $insert->execute();
             else:
                 print_r($is_valid);
             endif;
         }
-      
+        
         public static function insertCompany($name,$description){
             $rules = [
                 'name' => 'required|max_len,100|min_len,2',
@@ -55,7 +57,36 @@
             else:
                 print_r($is_valid);
             endif;    
-        }   
+        }
+
+        public static function insertEmployee($idUser, $idCompany, $position){
+            $rules = [
+                'idUser' => 'required|min_len,1|integer',
+                'idCompany' => 'required|min_len,1|integer',
+                'position' => 'required|min_len,2|alpha'
+            ];
+            $employee_date = [
+                'idUser' => $idUser,
+                'idCompany' => $idCompany,
+                'position' => $position
+            ];
+
+            $is_valid = GUMP::is_valid($employee_date,$rules);
+
+            if($is_valid === true):
+                $sql = 'INSERT INTO funcionario(id_usuario_fk,id_empresa_fk,cargo) VALUES (?,?,?)';
+                $insert = Conexao::getConn()->prepare($sql);
+                $insert->bindValue(1,$employee_date['idUser']);
+                $insert->bindValue(2,$employee_date['idCompany']);
+                $insert->bindValue(3,$employee_date['position']);
+
+                $insert->execute();
+            else:
+                print_r($is_valid);
+            endif;
+        }
+        
+       
     } 
     
     
